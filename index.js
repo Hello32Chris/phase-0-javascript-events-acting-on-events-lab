bee6b47598eeecefc6774bee77358d8d6df514aa
Christopher Orth <christopherorth430@yahoo.com>
2023-08-11 16:16:12 -0400

    events lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    game.style.backgroundColor = '#000000';
+    dodger.style.backgroundColor = "#FF69B4";
+  } else if (left === 0) {
+    dodger.style.backgroundColor = '#000000';
+    game.style.backgroundColor = "#FF69B4";
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+      }
+});
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(rightNumbers, 10);
+
+if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+    game.style.backgroundColor = '#000000';
+    dodger.style.backgroundColor = "#FF69B4";
+  } else if (left === 360) {
+    dodger.style.backgroundColor = '#000000';
+    game.style.backgroundColor = "#FF69B4";
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+});
